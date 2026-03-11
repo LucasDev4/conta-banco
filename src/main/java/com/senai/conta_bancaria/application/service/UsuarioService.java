56cfc514d07e9872fc5838250b48bba0c0248e21
Lucas Rodrigues Ferraz <rodriguessslucas68@gmail.com>
2026-03-11 16:38:56 -0300

    Add files via upload

--- src/main/java/com/senai/conta_bancaria/application/service/UsuarioService.java
+++ src/main/java/com/senai/conta_bancaria/application/service/UsuarioService.java
@@ -1,9 +1,24 @@
 package com.senai.conta_bancaria.application.service;
 
 import com.senai.conta_bancaria.domain.entity.Usuario;
+import com.senai.conta_bancaria.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UsuarioService {
 
-    public void cadastrarUsuario(Usuario usuario) {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
     }
 }
--- src/main/java/com/senai/conta_bancaria/domain/entity/Usuario.java
+++ src/main/java/com/senai/conta_bancaria/domain/entity/Usuario.java
@@ -1,6 +1,24 @@
 package com.senai.conta_bancaria.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
     private String email;
